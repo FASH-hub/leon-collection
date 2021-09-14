907083e87b2956441b6677f121d9002e79a22483
Léon FASHINGABO <53854@etu.he2b.be>
2021-09-14 17:28:49 +0200

    creating items

---
+++ js/collection.js
@@ -0,0 +1,26 @@
+const sport = [
+    football = {
+        favorite: "football",
+        nbPlayers: 11,
+        bestPlayer: "Cristiano Ronaldo",
+        favoriteTeam: "Manchester United <link>",
+        currentWorldChamp: "France",
+        currentEuropeanCham: "Italy"
+    },
+    formula1 = {
+        favoriteMotorSport: "Formula1",
+        currentWorldChamp: 'Lewis Hamilton',
+        fovoriteDriver: "Lewis Hamilton",
+        mostCapedChamp: ['Lewis Hamilton <pictures>', 'Michael Schumachel'],
+        f1Teams: "<link>"
+
+    },
+    workout = {
+        currentPractice: "gym workout",
+        gymName: "Jims",
+        location: "Avenue Louise..",
+        frequency: "4 times a week",
+        prices: "<link below .."
+    }
+
+];
