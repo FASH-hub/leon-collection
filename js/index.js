2a26215b9aa2a9a10350aa9bb550ea23194467bc
Léon FASHINGABO <53854@etu.he2b.be>
2021-09-25 20:33:53 +0200

    [updates: version 2] collection cards

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
 function accesToCard() {
 
     let div = document.querySelector('div');
-    let nav = document.createElement('nav');
+    let main = document.createElement('main');
+
     //loops on object "sport" containing sport celebreties.
     for (const elt of sport) {
 
-        let cardToLink = document.createElement('a');
-        let linkVal = document.createTextNode('');
 
-        //loops on object "links" containing differents links.
-        for (const alink of links) {
-            linkVal = alink.cardLink;
-        }
+        let section = document.createElement('section');
+        let image = document.createElement('img');
+        image.src = elt.picture;
+        section.appendChild(image);
 
-        cardToLink.href = linkVal;
-        console.log(cardToLink);
-        cardToLink.innerHTML = elt.picture;
-        nav.appendChild(cardToLink);
+        let title = document.createElement('h1');
+        title.innerText = elt.name;
+        section.appendChild(title);
 
+        let para = document.createElement('p');
+        let birthDetails = document.createTextNode(' Was born on ' + elt.birthDate + ' in ' +
+            elt.birthPlace + ' , ' + elt.country + '. ');
+        let family = document.createTextNode(elt.name + ' is ' + elt.spouse +
+            'and has' + elt.children + '. ');
+        let profession = document.createTextNode(elt.name + ' competes in ' +
+            elt.competitionLeague + ' for ' + elt.Team + ' and has ' + elt.championships);
+        para.append(birthDetails, family, profession);
+        section.appendChild(para);
+
+        let aside = document.createElement('aside');
+        let asideLink = document.createElement('a');
+        asideLink.setAttribute("href", elt.biography);
+        asideLink.innerHTML = 'Full biography';
+        aside.appendChild(asideLink);
+        section.appendChild(aside);
+
+        main.appendChild(section);
     }
-    div.appendChild(nav);
-    document.body.appendChild(div);
+    div.appendChild(main);
+
 }
 accesToCard();
